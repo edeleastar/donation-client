7f6e2556e8d792054653526254723e32bda61744
Eamonn de Leastar <edeleastar@wit.ie>
2016-11-12 07:25:30 +0000

    login in app uses donation-service login

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import {inject} from 'aurelia-framework';
+import DonationService from './services/donation-service';
+
+@inject(DonationService)
 export class App {
 
   email = 'marge@simpson.com';
@@ -5,9 +9,15 @@ export class App {
 
   loggedIn = false;
 
+  constructor(ds) {
+    this.donationService = ds;
+  }
+
   login(e) {
-    console.log(`Logging in ${this.email}`);
-    this.loggedIn = true;
+    console.log(`Trying to log in ${this.email}`);
+    const status = this.donationService.login(this.email, this.password);
+    this.prompt = status.message;
+    this.loggedIn = status.success;
   }
 
   logout() {
