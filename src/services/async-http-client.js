b4fd228bc2f612edd057ea5126a4c5890682c9bf
Eamonn de Leastar <edeleastar@wit.ie>
2016-11-17 16:00:15 +0000

    include jwt authentication via authenticate feature

--- src/services/async-http-client.js
+++ src/services/async-http-client.js
@@ -1,14 +1,43 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 import Fixtures from './fixtures';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {LoginStatus} from './messages';
 
-@inject(HttpClient, Fixtures)
+@inject(HttpClient, Fixtures, EventAggregator )
 export default class AsyncHttpClient {
 
-  constructor(httpClient, fixtures) {
+  constructor(httpClient, fixtures, ea) {
     this.http = httpClient;
     this.http.configure(http => {
       http.withBaseUrl(fixtures.baseUrl);
+    });
+    this.ea = ea;
+  }
+
+  authenticate(url, user) {
+    this.http.post(url, user).then(response => {
+      const status = response.content;
+      if (status.success) {
+        localStorage.donation = JSON.stringify(response.content);
+        this.http.configure(configuration => {
+          configuration.withHeader('Authorization', 'bearer ' + response.content.token);
+        });
+      }
+      this.ea.publish(new LoginStatus(status));
+    }).catch(error => {
+      const status = {
+        success: false,
+        message: 'service not avilable'
+      };
+      this.ea.publish(new LoginStatus(status));
+    });
+  }
+
+  clearAuthentication() {
+    localStorage.donation = null;
+    this.http.configure(configuration => {
+      configuration.withHeader('Authorization', '');
     });
   }
 
